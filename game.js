a9924528a26f7b18eb33e8c300db00f9263a84cc
shriram2 <91598962+shriram2@users.noreply.github.com>
2021-09-29 17:31:35 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,25 @@
+
+    var config = {
+        type: Phaser.AUTO,
+        width: 400,
+        height: 300,
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+
+    var game = new Phaser.Game(config);
+
+    function preload ()
+    {
+    }
+
+    function create ()
+    {
+    }
+
+    function update ()
+    {
+    }
