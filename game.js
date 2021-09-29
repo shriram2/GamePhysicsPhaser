279a33445e5d7bc1a04196f57254d8be444bca6e
shriram2 <91598962+shriram2@users.noreply.github.com>
2021-09-29 19:04:21 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 
     var config = {
         type: Phaser.AUTO,
-        width: 400,
-        height: 300,
+        width: 600,
+        height: 500, backgroundColor: 0x27ae60,
         scene: {
             preload: preload,
             create: create,
@@ -14,10 +14,17 @@
 
     function preload ()
     {
+
+     this.load.image('boy', 'asset/boy.png');
+    this.load.image('bomb', 'asset/bomb.png');
+
     }
 
     function create ()
     {
+
+    this.add.image(400, 300, 'boy');
+    this.add.image(300, 300, 'bomb');
     }
 
     function update ()
